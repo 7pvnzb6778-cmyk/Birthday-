9fbf43b1c91d8d7e73fa368b0d7af64fe45609b9
7pvnzb6778-cmyk <7pvnzb6778@privaterelay.appleid.com>
2026-09-25 00:33:27 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,39 @@
+const pages = [
+
+{
+    image:"images/1.jpg",
+    title:"🎂 Happy Birthday to you🎂",
+    text:"Happy Birthday to you ❤️"
+},
+
+{
+    image:"images/2.jpg",
+    title:"🫂",
+    text:"May your day be filled with joy and laughter🎂"
+},
+
+{
+    image:"images/3.jpg",
+    title:"💐",
+    text:"Wishing you a wonderful birthday and a fantastic year ahead\n ❤️"
+},
+
+{
+    image:"images/4.jpg",
+    title:"💘",
+    text:"I wish you happiness,joy,and\nmany more years of good health🤍"
+},
+
+{
+    image:"images/5.jpg",
+    title:"💘",
+    text:"finally,I hope you and my aunt piyatida\nlove each other forever🥳"
+},
+
+{
+    image:"images/6.jpg",
+    title:"❤️",
+    text:"I made this for you as a birthday gift❤️"
+}
+
+];
